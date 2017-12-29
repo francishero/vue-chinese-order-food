8858db89f3a4fb67ce5eafff8a8da186d95473a4
Francis Miyoba <francismiyoba@gmail.com>
2017-12-29 01:06:36 -0800

    Custom resolve routes for components. So we can just use from 'components/component/file'

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -34,8 +34,9 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      'src': path.resolve(__dirname, '../src'),
+      'common': path.resolve(__dirname, '../src/common'),
+      'components': path.resolve(__dirname, '../src/components')
     }
   },
   module: {
